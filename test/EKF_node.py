555199aa8868e4462b10535cac026bed6f313377
Hyunkil Hwang <83130841+Hyunkil76@users.noreply.github.com>
2023-09-11 19:11:46 +0900

    Add ekf_path publish

--- test/EKF_node.py
+++ test/EKF_node.py
@@ -1,19 +1,25 @@
 import numpy as np
+import transforms3d.quaternions as quat
+
+from tf_transformations import euler_from_quaternion
 from filterpy.kalman import ExtendedKalmanFilter
 from filterpy.common import Q_discrete_white_noise
-import transforms3d.quaternions as quat
-from tf_transformations import euler_from_quaternion
 
 import rclpy
 from rclpy.node import Node
 from rclpy.clock import Clock
+
 from geometry_msgs.msg import TransformStamped
 from tf2_msgs.msg import TFMessage
 from sensor_msgs.msg import NavSatFix
 from sensor_msgs.msg import Imu
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped
+
 from numpy.linalg import inv
 from scipy.spatial.transform import Rotation
 from pyproj import Transformer
+
 
 class EKF_node(Node):
     def __init__(self):
@@ -30,7 +36,10 @@ class EKF_node(Node):
             self.imu_callback,
             10
         )
-        self.pub = self.create_publisher(TFMessage, '/tf', 10)
+        self.pub_tf = self.create_publisher(TFMessage, '/tf', 10)
+        self.pub_path = self.create_publisher(Path, '/ekf_path', 10)
+        self.path_list = []
+        self.path_msg = Path()
 
         self.positions = np.empty((0,3))
         self.init_east = 0
@@ -61,19 +70,22 @@ class EKF_node(Node):
 
             utm_e, utm_n = self.lat_lon_to_utm(latitude, longitude)
 
+            # Set Initial Position
             if (self.is_fix_init == True):
                 self.init_east = utm_e
                 self.init_north = utm_n
                 self.init_alt = altitude
                 self.is_fix_init=False
 
+            # Compute Relative Position (N, W, Z) axis
             diff_e = -(utm_e-self.init_east)
             diff_n = utm_n-self.init_north
             diff_a = altitude-self.init_alt
-
             pos_array = np.array([diff_n, diff_e, diff_a])
+
+            # Change (N, W, Z) axis to (X, Y, Z) axis
             filtered_position = inv(self.relative_matrix) @ pos_array
-            print(filtered_position)
+            # print(filtered_position)
             self.x = filtered_position[0]
             self.y = filtered_position[1]
             self.z = filtered_position[2]
@@ -89,27 +101,29 @@ class EKF_node(Node):
         q_z = data.orientation.z
         q_w = data.orientation.w
 
+        # Set Initial Heading
         if (self.is_imu_init == True):
-
             self.init_quart = np.array([q_w, q_x, q_y, q_z])
-
             self.relative_matrix = Rotation.from_quat([q_x, q_y, q_z, q_w]).as_matrix()
-            print(self.relative_matrix)
+            # print(self.relative_matrix)
             self.is_imu_init=False
 
+        # Compute Relative Orientaion
         current_q = np.array([q_w, q_x, q_y, q_z])
-
         delta_q = quat.qmult(quat.qinverse(self.init_quart), current_q)
-
         self.ox = delta_q[1]
         self.oy = delta_q[2]
         self.oz = delta_q[3]
         self.ow = delta_q[0]
 
+        # Predict & Update Position
         z = np.array([[self.x, self.y, self.z, self.ox, self.oy, self.oz, self.ow]]).T
         self.localizer.update(z, HJacobian=self.H, Hx=self.h)
-        print(self.localizer.x)
+        self.localizer.predict()
+        # print(self.localizer.x)
+        
 
+        # Publish TF
         msg = TransformStamped()
         msg.header.stamp = data.header.stamp
         msg.header.frame_id = 'odom'
@@ -124,9 +138,25 @@ class EKF_node(Node):
 
         tf_msg = TFMessage()
         tf_msg.transforms = [msg]
-        self.pub.publish(tf_msg)
-        self.localizer.predict()
+        self.pub_tf.publish(tf_msg)
 
+        # Publish EKF_path
+        pose_msg = PoseStamped()
+        pose_msg.header.stamp = data.header.stamp
+        pose_msg.header.frame_id = 'map'
+        pose_msg.pose.position.x = float(self.localizer.x[0])
+        pose_msg.pose.position.y = float(self.localizer.x[1])
+        pose_msg.pose.position.z = float(self.localizer.x[2])
+        pose_msg.pose.orientation.x = float(self.localizer.x[3])
+        pose_msg.pose.orientation.y = float(self.localizer.x[4])
+        pose_msg.pose.orientation.z = float(self.localizer.x[5])
+        pose_msg.pose.orientation.w = float(self.localizer.x[6])
+
+        self.path_list.append(pose_msg)
+        # print(self.path_list)
+        self.path_msg.header = pose_msg.header
+        self.path_msg.poses = self.path_list
+        self.pub_path.publish(self.path_msg)
 
 def main():
     rclpy.init()
