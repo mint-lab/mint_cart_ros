78887fe1f9f3a3aacd780360a181f379c5c705f4
dongwookheo <hdwook3918@gmail.com>
2024-05-16 17:15:13 +0900

    Feat: Add the function to find and cluster a flat part within the data to obtain an average

--- test/test_filter.py
+++ test/test_filter.py
@@ -6,6 +6,7 @@ import sys
 import matplotlib.pyplot as plt
 
 def read_pressure_data(file_path: str) -> list:
+    """Read a barometric data file and return the pressure data as a list"""
     pressure_list = []
     try:
         with open(file_path, newline='') as f:
@@ -23,20 +24,64 @@ def read_pressure_data(file_path: str) -> list:
 
     return pressure_list
 
-def moving_average_filter(data_list: deque, measurement):
+def moving_average_filter(data_list: deque, measurement: float, window_size: int) -> tuple[float, deque]:
+    """Apply a moving average filter to the pressure data"""
     # n = len(data_list)
     # for i in range(n - 1):
     #     data_list[i] = data_list[i + 1]
     # data_list[n - 1] = measurement
-    data_list.popleft()
+    if len(data_list) == window_size:
+        data_list.popleft()
     data_list.append(measurement)
     average = np.mean(data_list)
     return average, data_list
 
-def plot_data(measurement, filtered_data):
-    plt.figure(figsize=(10, 5))
+def find_flat_section(data_list, threshold=0.0002):
+    """Find the flat section in the data"""
+    gradient = np.gradient(data_list) # np.gradient vs np.diff ??
+    flat_section = np.where(np.abs(gradient) < threshold)[0] # np.where returns tuple
+    return flat_section
+
+def calculate_cluster(flat_section, data_list, tolerance=100):
+    """Find clusters and calculate the average of each cluster"""
+    cluster_list = []
+    current_cluster = []
+
+    for i in range(len(flat_section)):
+        if i == 0:
+            current_cluster.append(flat_section[i])
+        else:
+            if abs(flat_section[i] - current_cluster[-1]) <= tolerance:
+                current_cluster.append(flat_section[i])
+            else:
+                cluster_list.append(current_cluster)
+                current_cluster = [flat_section[i]]
+
+    # Prevent missing last cluster
+    if current_cluster:
+        cluster_list.append(current_cluster)
+
+    # Get average in clusters
+    cluster_average_list = []
+    for cluster in cluster_list:
+        value_list = [data_list[i] for i in cluster]
+        average = np.mean(value_list)
+        cluster_average_list.append(average)
+
+    return cluster_list, cluster_average_list
+
+def plot_data(measurement, filtered_data, flat_section, cluster_list, cluster_average_list):
+    """Plot the measurement data, the filtered data and the flat section"""
+    plt.figure(figsize=(20, 10))
     plt.plot(measurement, 'r*', label='Measured')
     plt.plot(filtered_data, 'b-', label='Filtered')
+    plt.plot(flat_section, [filtered_data[i] for i in flat_section],'g+', label='Flat')
+
+    # Put the average of the cluster into the text
+    for cluster, average in zip(cluster_list, cluster_average_list):
+        cluster_mid_point = cluster[len(cluster) // 2]
+        plt.text(cluster_mid_point, average, f'{average}', fontsize=10, ha='center', va='bottom')
+
     plt.title('Measure Pressure v.s. Moving Average Filter Values')
     plt.xlabel('Index')
     plt.ylabel('Pressure [hpa]')
@@ -57,16 +102,20 @@ def main():
     window_size = 100
     print(f'window_size={window_size}')
     for measurement in pressure_data_list:
-        if len(pressure_window) < window_size:
-            average, pressure_window = measurement, deque([measurement] * window_size, maxlen=window_size)
-        else:
-            average, pressure_window = moving_average_filter(pressure_window, measurement)
+        average, pressure_window = moving_average_filter(pressure_window, measurement, window_size)
         filtered_data_list.append(average)
 
     print(len(filtered_data_list), filtered_data_list[0:5])
 
+    # Find flat section
+    flat_section = find_flat_section(filtered_data_list)
+    print(flat_section, type(flat_section))
+
+    # Find clusters and calculate average of each cluster
+    cluster_list, cluster_average_list = calculate_cluster(flat_section, pressure_data_list)
+
     # Create the plot
-    plot_data(pressure_data_list, filtered_data_list)
+    plot_data(pressure_data_list, filtered_data_list, flat_section, cluster_list, cluster_average_list)
 
 if __name__ == "__main__":
     main()
